9142ff3db850d637914c987fad484c86b187d1f6
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-05-17 19:50:59 +0530

    Add files via upload

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
 
-const Avatar = () => {
+const Avatar = ({ children, backgroundColor, size, color, cursor }) => {
+  const style = {
+    backgroundColor,
+    width: size,
+    height: size,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: color || 'black',
+    borderRadius: '50%', // Set border radius to 50% for a circle
+    fontSize: '16px',
+    textAlign: 'center',
+    cursor: cursor || 'pointer',
+    textDecoration: 'none'
+  };
+
   return (
-    <div>
-      
+    <div style={style}>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Avatar
+export default Avatar;
+
+
+
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -4,39 +4,37 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/StackOverflowIcon.png';
 import search from '../../assets/SearchIcon.png';
 import Avatar from '../../components/Avatar/Avatar';
-import Button from '../../components/Button/Button';
-import './navbar.css'
-
-
-
+import './navbar.css';
 
 const Navbar = () => {
-
-var user = null
+  const user = 123;
 
   return (
     <nav className="navbar">
       <Link to="/" className="nav-item nav-logo">
         <img src={logo} alt="logo" className="logo" width='180' />
       </Link>
-      <Link to = '/' className='nav-item nav-btn'> About </Link>
-      <Link to = '/' className='nav-item nav-btn'> Products </Link>
-      <Link to = '/' className='nav-item nav-btn'>For Teams</Link>
-
-      <form>
+      <div className="nav-items">
+        <Link to='/' className='nav-item nav-btn'> About </Link>
+        <Link to='/' className='nav-item nav-btn'> Products </Link>
+        <Link to='/' className='nav-item nav-btn'>For Teams</Link>
+      </div>
+      <form className="search-form">
         <input type="text" placeholder='Search...' />
         <img src={search} alt='search' width='18' className='search-icon'/>
+        <div className="avatar-wrapper">
+          {user == null ? (
+            <Link to='/Auth' className='nav-item nav-links'>Log In</Link>
+          ) : (
+            <>
+              <Avatar backgroundColor='#009dff' size="30px" color="white">
+  <Link to='/' style={{ color: "white", textDecoration: "none" }}>A</Link>
+</Avatar>
 
-        {user == null ? 
-        <Link to= '/Auth' className='nav-item nav-links'>Log In</Link> :
-         <>
-         <Link to='/'><Avatar> A </Avatar></Link>
-         <Button>Log Out</Button>
-
-         </>
-         
-         }
-          
+              <button className='nav-item nav-links'><span style={{ whiteSpace: 'nowrap' }}>Log Out</span></button>
+            </>
+          )}
+        </div>
       </form>
     </nav>
   );

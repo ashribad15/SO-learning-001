659b86d8614667d9a767a163815c661060009524
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-05-23 00:35:25 +0530

    Update Auth.jsx

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -12,9 +12,7 @@ const Auth = () => {
 
   return (
     <section className='auth-section'>
-      {
-        isSignup && <AboutAuth/>
-      }
+      {isSignup && <AboutAuth />}
       <div className='auth-container'>
         {!isSignup && <img src={icon} alt='Stack Overflow' className='login-logo' />}
         <form>
@@ -29,18 +27,17 @@ const Auth = () => {
             <input type="email" name='email' id='email' />
           </label>
           <label htmlFor="password">
-            <div style={{display:"flex", justifyContent :"space-between"}}>
-              <p style={{color:"#007ac6", fontSize:"13px"}}>Password</p>
-              {!isSignup && <h4>Forgot Password?</h4>}
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <h4>Password</h4>
+              {!isSignup && <h4 style={{ color: "#007ac6", fontSize: "13px" }}>Forgot Password?</h4>}
             </div>
             <input type="password" name='password' id='password' />
-            {isSignup && <p style={{ color: '#007ac6', fontSize: '13px' }}> Must contain 8+ characters, including at least 1 letter and 1 number</p> }
+            {isSignup && <p style={{ color: '#007ac6', fontSize: '13px' }}>Must contain 8+ characters, including at least 1 letter and 1 number</p>}
           </label>
           {isSignup && (
             <label htmlFor='check'>
               <input type='checkbox' id="check" />
-              <p style={{ color: '#007ac6', fontSize: '13px' }}> A strong password can be memorable to you but <br />
-              nearly impossible for someone else to guess.</p>
+              <p style={{ color: '#007ac6', fontSize: '13px' }}>A strong password can be memorable to you but nearly impossible for someone else to guess.</p>
             </label>
           )}
           <button type="submit" className="auth-btn">
@@ -48,8 +45,7 @@ const Auth = () => {
           </button>
           {isSignup && (
             <p style={{ color: '#007ac6', fontSize: '13px' }}>
-              By checking this "Sign Up",<br/>
-              you are agreeing to our <span style={{ color: '#007ac6', fontSize: '13px' }}>terms and conditions</span> and <span style={{ color: '#007ac6', fontSize: '13px' }}>cookie policy</span>.
+              By checking this "Sign Up", you are agreeing to our <span style={{ color: '#007ac6' }}>terms and conditions</span> and <span style={{ color: '#007ac6' }}>cookie policy</span>.
             </p>
           )}
         </form>
@@ -65,4 +61,3 @@ const Auth = () => {
 };
 
 export default Auth;
-
